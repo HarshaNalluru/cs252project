28a7688c33a69e60f8d63b71c998745667dfba61
abhigun <abhishek128959@outlook.com>
2016-11-09 23:46:55 +0530

    added floating button for ip input
    added floating button

--- ReceiveSms/app/src/main/java/com/example/harsha/receivesms/MainActivity.java
+++ ReceiveSms/app/src/main/java/com/example/harsha/receivesms/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.harsha.receivesms;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.reflect.TypeToken;
+import com.yarolegovich.lovelydialog.LovelyTextInputDialog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,49 +28,38 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
-    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final SharedPreferences sharedPreferences = getSharedPreferences("ipaddress", Context.MODE_PRIVATE);
-        sharedPreferences.getString("ip","");
-         editText = (EditText)findViewById(R.id.ipaddress);
-         submit = (Button)findViewById(R.id.button);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("ip", editText.getText().toString());
-                editor.commit();
-               // String ip = sharedPreferences.getString("ip", "");
-                //sendmessage(ip, "apkr09@gmail.com:subject3:body4:9657782929");
-            }
-        });
+        sharedPreferences.getString("ip", "");
+        FloatingActionButton floatingActionButton =(FloatingActionButton)findViewById(R.id.fab);
+        if (floatingActionButton != null) {
+            floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new LovelyTextInputDialog(MainActivity.this, R.style.EditTextTintTheme)
+                            .setTopColorRes(R.color.darkDeepOrange)
+                            .setTitle("Add the ip address")
+                            .setMessage("Ex:192.168.0.112")
+                            .setIcon(R.drawable.ic_account_check_white_24dp)
 
+                            .setConfirmButton(android.R.string.ok, new LovelyTextInputDialog.OnTextInputConfirmListener() {
+                                @Override
+                                public void onTextInputConfirmed(String text) {
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("ip", text);
+                                    editor.commit();
+                                }
+                            })
+                            .show();
+                }
+            });
+
+        }
 
 
     }
-    private void sendmessage(String ip,String message) {
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(ip)
-                .build();
-        DataService dataService =restAdapter.create(DataService.class);
 
-        Callback callback = new Callback() {
-
-            @Override
-            public void success(Object o, Response response) {
-                APIResponse ticketids = (APIResponse)o;
-                ticketids.getResult();
-            }
-
-            @Override
-            public void failure(RetrofitError retrofitError) {
-                Log.v("failure","fail");
-            }
-        };
-        dataService.getEvents(message, callback);
-    }
 }
