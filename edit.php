76817e5d10db5d9c817fc3140ee723df125a0065
Harsha Nalluru <harsha.nalluru1997@gmail.com>
2016-11-09 22:59:16 +0000

    added script for updating password

---
+++ edit.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+
+include_once 'dbconnect.php';
+
+//set validation error flag as false
+$error = false;
+
+//check if form is submitted
+    //var_dump(bin2hex($password));
+if (isset($_POST['confirm'])) {
+    $password =  mysqli_real_escape_string($con, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+    $id = $_SESSION['usr_id'];
+    if(strlen($password) < 6) {
+        $error = true;
+        $password_error = "Password must be minimum of 6 characters";
+    }
+    if($password != $cpassword) {
+        $error = true;
+        $cpassword_error = "Password and Confirm Password doesn't match";
+    }
+    $pass = md5($password);
+    if (!$error) {
+        if(mysqli_query($con, "UPDATE users SET password='$pass' WHERE id ='$id'")) {
+            $successmsg = "Successfully Updated! <a href='index.php'>Click here to check mails</a>";
+        } else {
+            $errormsg = "Error in updating...Please try again later!";
+        }
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Settings</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+</head>
+<body>
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <!-- add header -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">CS252-PROJECT</a>
+        </div>
+        <!-- menu items -->
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="nav navbar-nav navbar-right">
+                <?php if (isset($_SESSION['usr_id'])) { ?>
+                <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
+                <li><a href="edit.php">Settings</a></li>
+                <li><a href="logout.php">Log Out</a></li>
+                <?php } else { ?>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 well">
+            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="signupform">
+                <fieldset>
+                    <legend>Change Password</legend>
+
+                    <div class="form-group">
+                        <label for="name">Password</label>
+                        <input type="password" name="password" placeholder="Password" required class="form-control" />
+                        <span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Confirm Password</label>
+                        <input type="password" name="cpassword" placeholder="Confirm Password" required class="form-control" />
+                        <span class="text-danger"><?php if (isset($cpassword_error)) echo $cpassword_error; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" name="confirm" value="Confirm" class="btn btn-primary" />
+                    </div>
+                </fieldset>
+            </form>
+            <span class="text-success"><?php if (isset($successmsg)) { echo $successmsg; } ?></span>
+            <span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+        
+        </div>
+    </div>
+</div>
+<script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
